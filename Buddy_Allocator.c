a68c1d2463adfd4399df10d98a3a15b09d8fdf1c
GiuseMisu <130069380+GiuseMisu@users.noreply.github.com>
2023-07-13 18:10:30 +0200

    Add files via upload

--- Buddy_Allocator.c
+++ Buddy_Allocator.c
@@ -14,10 +14,68 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
 
 #include "Buddy_Allocator.h"
 
+int get_parent_index(int index){
+  return (int)((index) / 2);
+}
 
+int from_index_to_level(int index){
+  return (int)log2(index);
+}
+
+int get_left_child_index(int index){
+  return (int)((index) * 2 +1); 
+}
+
+int get_right_child_index(int index){
+  return (int)((index) * 2 + 2); 
+}
+
+int offset_from_first(int index){
+  return (int)(index - (1 << index));
+}
+
+int first_index_from_level(int level){
+  return (int)(1 << level);
+}
+
+void buddy_allocator_init(Buddy_allocator * buddy_allocator, void * buffer, Bit_map * bitmap, int levels, int size){
+  buddy_allocator->mem = buffer;
+  buddy_allocator->bitmap = bitmap;
+  buddy_allocator->levels = levels;
+  buddy_allocator->size = size;
+}
+
+int search_free_block(Bit_map * bitmap, int level){
+  int starting_index = first_index_from_level(level) - 1;
+  int ending_index = first_index_from_level(level + 1) - 1;
+
+  //while loop fino a che non trovo un blocco libero
+  int i = starting_index;
+  while(i < ending_index){
+    if(bit_map_get(bitmap, i) == 1){
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
+int search_free_level(Buddy_allocator * buddy_allocator, int dim){
+  int lvl = buddy_allocator->levels - 1; //-1 perchè l'indice parte da 0
+  int starting_dim = buddy_allocator->minimum_block_size;
+  while(lvl >= 0){
+    if(dim <= starting_dim){
+      return lvl;
+    }
+    lvl--;
+    starting_dim = starting_dim * 2;
+  }
+  return lvl;
+}
 
 
 
--- Buddy_Allocator.h
+++ Buddy_Allocator.h
@@ -5,14 +5,31 @@
 #include "bitmap.h"
 
 typedef struct {
-
-    int livelli; // livelli dell'albero
-    int size; // dimensione della memoria allocata
-    int min_size; // dimensione minima di un blocco
-    void *mem; // puntatore alla memoria allocata
     Bit_map *bitmap; // puntatore alla bitmap (guarda bitmap.h)
+    void *mem; // puntatore alla memoria allocata
+    int levels; // livelli dell'albero
+    int size; // dimensione della memoria allocata //forse da rimuovere tanto lo ottieni da mem 
+    int minimum_block_size; // dimensione minima che può assumere blocco
     int *free_blocks; // array di interi che tiene traccia dei blocchi liberi
+}Buddy_allocator;
 
+int get_parent_index(int index);
 
+int from_index_to_level(int index);
 
-}BuddyAllocator;
+int get_left_child_index(int index);
+
+int get_right_child_index(int index);
+
+int offset_from_first(int index);
+
+int first_index_from_level(int level);
+
+// inizializza la struttura dati buddy allocator
+void buddy_allocator_init(Buddy_allocator * buddy_allocator, void * buffer, Bit_map * bitmap, int levels, int size);
+
+//dato un livello restituisce la prima posizione libera
+int search_free_block(Bit_map * bitmap, int level);
+
+//trova il livello contenente il blocco di dimensione minima in grado di contenere elemento di dimensioni dim
+int search_free_level(Buddy_allocator * buddy_allocator, int dim);
