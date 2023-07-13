a9177b923a0189bcd12354edc7538ceafef723ba
GiuseMisu <130069380+GiuseMisu@users.noreply.github.com>
2023-07-13 16:56:18 +0200

    Add files via upload

--- bitmap.h
+++ bitmap.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <stdint.h>
 
-
 typedef struct {
-    int dim;
+    int dim;    
     int num_bit;
     uint8_t * data;
 } Bit_map;
@@ -14,13 +13,23 @@ param:
     buff: puntatore al buffer da utilizzare
     dim_buffer: dimensione del buffer
 */
-
 void bit_map_init(Bit_map * bm, uint8_t * buff ,int dim_buffer);
 
-void bit_map_destroy(Bit_map * bit_map);
-
+/*
+setta il bit in posizione index in base al valore di boolean
+param:
+    bm: puntatore alla struttura dati da utilizzare
+    index: posizione del bit da settare
+    boolean: valore del bit da settare, 0 o 1
+*/
 void bit_map_set(Bit_map * bit_map, int index, int boolean);
 
+/*
+ritorna il valore del bit in posizione index
+param:
+    bm: puntatore alla struttura dati da utilizzare
+    index: posizione del bit da ritornare
+*/
 int bit_map_get(Bit_map * bit_map, int index);
 
 
