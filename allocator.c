9dfb32d0be8f5f2593051092e1cb6247f8ade7ce
Giuseppe Misuraca <130069380+GiuseMisu@users.noreply.github.com>
2023-09-27 16:11:32 +0200

    Combined two deallocation functions into one, added a data structure
    Le due funzioni di deallocazione tramite munmap e tramite la mia implementazione sono state unificate in un'unica funzione (deallocator).
    Aggiunta una struttura dati per Allocatore (Allocator_strc) 
    Aggiornati i test per allocatore

--- allocator.c
+++ allocator.c
@@ -15,14 +15,16 @@
 
 
 //funzione che inizializza allocator se date bit_map, buffer e buddy_allocator
-void allocator_initialize(Bit_map * bitmap, void * buffer, Buddy_allocator * buddy_allocator, void * buffer_buddy, int num_levels, int min_block_size, int bitset_size){
+void allocator_initialize(Allocator_strc * alloc,Bit_map * bitmap,  Buddy_allocator * buddy_allocator ,void * buffer, void * buffer_buddy, int num_levels, int min_block_size, int bitset_size){
+	alloc -> buddy = buddy_allocator;
+	
     bit_map_init(bitmap, buffer, bitset_size);
     buddy_allocator_init(buddy_allocator, buffer_buddy, bitmap, num_levels, min_block_size);
 }
 
 
 //funzione che alloca la memoria
-void *allocator(int size, Buddy_allocator * buddy_allocator){
+Allocator_strc *allocator(int size, Allocator_strc * alloc){
     if(size < 1){
         return NULL;
     }
@@ -30,7 +32,7 @@ void *allocator(int size, Buddy_allocator * buddy_allocator){
         size = size + sizeof(size);//bisogna aggiungere alla size anche la dimensione 
         //in base alla dimensione richiesta decidiamo se effetturare allocazione tramite mmap o allocator
         if(size > page_dim/4){
-            printf("la dimensione è %d, quindi è più grande della dimensione di un quarto di pagina %d \n", size, page_dim/4);
+            printf("\nla dimensione è %d, quindi è più grande della dimensione di un quarto di pagina %d \n", size, page_dim/4);
             void * pointer = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
             if(pointer == MAP_FAILED){
@@ -39,70 +41,71 @@ void *allocator(int size, Buddy_allocator * buddy_allocator){
                 return NULL;
             }
             else{
-                return pointer;
+				alloc->ptr = pointer;//mi salvo nella struttura Allocator_strc il puntatore alla zona appena allocata
+                return alloc;
             }
         }
         else{
-            void *a = buddy_allocator_alloc(buddy_allocator, size); 
+			Buddy_allocator * buddy = alloc->buddy;
+            void *a = buddy_allocator_alloc(buddy, size); 
             if(a == NULL){
                 perror("error allocator: buddy_allocator_alloc failed");
                 return NULL;
             }
             else{
-                return a;
+				alloc->ptr = a;//mi salvo nella struttura Allocator_strc il puntatore alla zona appena allocata
+                return alloc;
             } 
         }
     }
 }
 
-//funzione che dealloca la memoria se allocazione fatta con mmap
-//se allocazione stata fatta tramite mmap serve ptr e dim 
-int deallocator_mmap(void *ptr, int dim){
-    if(dim < 1 && ptr != NULL){
+
+//nuova versione della funzione deallocator, viene chiamata per deallocare a prescindere dalla tipologia di allocazione 
+int deallocator(Allocator_strc * alloc, int dim){
+	
+	if(dim < 1|| alloc == NULL){
         return -1;
     }
-
-    else{
-        if(dim > page_dim/4){//let's use the munmap
-            dim = dim + sizeof(dim);
-            if(munmap(ptr, dim) == -1){
-                perror("munmap");
-                return -1;
-            }
-        }
-        //else stai sbagliando a chiamare la funzione devi deallocare con buddy
-        else{
-            return -1;
-        }
-    }
+   
+    void * puntatore = alloc->ptr;
+    Buddy_allocator* b = alloc->buddy;
+    
+    if(puntatore == NULL || b == NULL){
+		return -1;
+	}
+    
+    uint8_t *first_index_buddy = &b->mem[0];//voglio sapere indirizzo di memoria del primo elemento del buffer di memoria del buddy
+    uint8_t *last_index_buddy = first_index_buddy + ( 1 << (b->levels) ) * b->minimum_block_size - 1; //mi calcolo ultimo indirizzo di memoria del buddy conoscendo il numero di livelli e la dimensione minima del blocco
+	
+	//controlla il puntatore restituisto da allocazione, per capire se questo punta a indirizzo che è dentro spazio degli indirizzi del buddy o se fuori buddy (per comprendere se dentro buddy prendo indirizzo del primo elemento della memoria del buddy e dell'ultimo)
+	//se dentro allora dealloca con buddy, se fuori con munmap
+	    
+	if(	(uint8_t *) puntatore >= first_index_buddy && (uint8_t *)puntatore<= last_index_buddy){//caso in cui deallochi con buddy
+		printf("Il puntatore pointer si trova all'interno del range del buddy.\n");
+		buddy_allocator_free_buddy(b,puntatore); 
+	}
+	else{//caso munmap
+		printf("Il puntatore pointer NON si trova all'interno del range del buddy.\n");
+		dim = dim + sizeof(dim);
+		if(munmap(puntatore, dim) == -1){ 
+			perror("munmap");
+			return -1;
+		}
+	}	
 }
 
 
-//funzione che dealloca la memoria se allocazione fatta tramite allocator
-//se allocazione fatta con mia implementazione serve passare il buddy e il puntatore restituito da buddy_allocator_alloc()
-int deallocator_buddy(Buddy_allocator * buddy_allocator, void *ptr, int dim){
-    if(dim < 0){
-        return -1;
-    }
-    else{
-        if(dim <= page_dim/4){//let's use the buddy
-            buddy_allocator_free_buddy(buddy_allocator,ptr); 
-        }
-        else{//else stai sbagliando a deallocare devi usare munmap
-            return -1;
-        }
-       
-    }
-}
 
 
 //Funzione che alloca la memoria e la inizializza con il valore num
-void * allocator_simil_calloc(int size, int num, Buddy_allocator * buddy_allocator){
+void * allocator_simil_calloc(int size, int num, Allocator_strc * alloc){
     if(size < 1){
         return NULL;
     }
     else{
-        void *pointer = allocator(size, buddy_allocator);
+		Buddy_allocator * buddy = alloc->buddy;
+        void *pointer = allocator(size, alloc)->ptr;
         if(pointer == NULL){
             perror("error allocator_simil_calloc: allocator failed");
             return NULL;
--- allocator.h
+++ allocator.h
@@ -1,18 +1,21 @@
 #pragma once
 
+
+typedef struct {
+    Buddy_allocator * buddy;
+    void * ptr; //puntatore alla zona di memoria allocata, restituito da funzione allocator a prescindere dalla tipologia di allocazione(se mmap o con buddy)
+}Allocator_strc;
+
 //Funzione che inizializza allocator
-void allocator_initialize(Bit_map * bitmap, void * buffer, Buddy_allocator * buddy_allocator,  void * buffer_buddy, int num_levels, int min_block_size, int bitset_size);
+void allocator_initialize(Allocator_strc * alloc, Bit_map * bitmap, Buddy_allocator * buddy_allocator, void * buffer, void * buffer_buddy, int num_levels, int min_block_size, int bitset_size);
 
 //Funzione che alloca la memoria, tipologia di allocazione dipende dalla dimensione richiesta
 //Se dim minore di 1/4 della dimensione di una pagina allora si usa la mia implementazione, altrimenti si usa mmap
-void *allocator(int size, Buddy_allocator * buddy_allocator);
+Allocator_strc *allocator(int size,  Allocator_strc * alloc);
 
-//funzione che dealloca la memoria se stata allocata con mmap (dim > 1/4 page_size)
-int deallocator_mmap(void *ptr, int dim);
-
-//Funzione che dealloca memoria se stata allocata tramite mia impl
-//se allocazione fatta con mia implementazione serve passare il buddy e il puntatore restituito da buddy_allocator_alloc()
-int deallocator_buddy(Buddy_allocator * buddy_allocator, void *ptr, int dim);
-
+//funzione che dealloca la memoria precedentemente allocata a prescindere dalla tipologia di allocazione effettuata
+int deallocator(Allocator_strc * alloc, int dim);
+	
 //Funzione che alloca la memoria e la riempie con il valore passato da utente
-void * allocator_simil_calloc(int size, int num, Buddy_allocator * buddy_allocator);
+void * allocator_simil_calloc(int size, int num, Allocator_strc * alloc);
+
